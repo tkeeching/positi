147aabaccbe46049b57590be5d29f9d05d715cf5
James Mah <junyeumah@gmail.com>
2020-08-02 17:29:46 +1000

    integrate storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { IonApp } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
@@ -8,6 +8,7 @@ import InspireMe from "./pages/InspireMe";
 import CustomiseCompliments from "./pages/CustomiseCompliments";
 import CustomiseInspirations from "./pages/CustomiseInspirations";
 import Schedule from "./pages/Schedule";
+import { useStorage } from "@capacitor-community/react-hooks/storage";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -27,27 +28,46 @@ import "@ionic/react/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+import { initialise } from "./utils/initialise";
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <Route path="/" component={Home} exact={true} />
-      <Route path="/compliment-me" component={ComplimentMe} exact={true} />
-      <Route path="/inspire-me" component={InspireMe} exact={true} />
-      <Route
-        path="/customise-compliments"
-        component={CustomiseCompliments}
-        exact={true}
-      />
-      <Route
-        path="/customise-inspirations"
-        component={CustomiseInspirations}
-        exact={true}
-      />
-      <Route path="/schedule" component={Schedule} exact={true} />
-      <Route render={() => <Redirect to="/" />} />
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const { get, set } = useStorage();
+
+  useEffect(
+    () => {
+      get("positi.initialised").then((value) => {
+        console.log("value: ", value);
+        if (value === null) {
+          set("positi.initialised", "true");
+          initialise();
+        }
+      });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/compliment-me" component={ComplimentMe} exact={true} />
+        <Route path="/inspire-me" component={InspireMe} exact={true} />
+        <Route
+          path="/customise-compliments"
+          component={CustomiseCompliments}
+          exact={true}
+        />
+        <Route
+          path="/customise-inspirations"
+          component={CustomiseInspirations}
+          exact={true}
+        />
+        <Route path="/schedule" component={Schedule} exact={true} />
+        <Route render={() => <Redirect to="/" />} />
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
